19ff720c971fe6829520ff1eb0ffaa024cfb824e
rahuls1988 <73811701+rahuls1988@users.noreply.github.com>
2022-10-10 15:18:41 +0530

    Add files via upload

---
+++ module5-solution/script.js
@@ -0,0 +1,26 @@
+// ******************************* 
+// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+
+
+
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
+
+
+
